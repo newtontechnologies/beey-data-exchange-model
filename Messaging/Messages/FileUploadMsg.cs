0aac14cf33076c67d3e51cce731bc986b24055ca
Ladislav Seps <ladislav.seps@newtontech.cz>
2019-05-15 14:16:10 +0200

    Added support for transcribing withou saving media file & trsx

--- Messaging/Messages/FileUploadMsg.cs
+++ Messaging/Messages/FileUploadMsg.cs
@@ -10,6 +10,7 @@ namespace BeeyApi.Messaging.Messages
     public partial class FileUploadMsg : Message<FileUploadMsg.MessageKind>
     {
         public long? FileOffset { get; internal set; }
+        public UploadConfig Config { get; set; }
 
         [Flags]
         public enum MessageKind
@@ -19,6 +20,20 @@ namespace BeeyApi.Messaging.Messages
             Failed,
             IsTerminated = Completed | Failed,
             UploadedBytes,
+        }
+
+        public class UploadConfig
+        {
+            public UploadConfig(bool transcribe, bool saveMedia, bool saveTrsx)
+            {
+                Transcribe = transcribe;
+                SaveMedia = saveMedia;
+                SaveTrsx = saveTrsx;
+            }
+
+            public bool Transcribe { set; get; }
+            public bool SaveTrsx { set; get; }
+            public bool SaveMedia { set; get; }
         }
     }
 }
