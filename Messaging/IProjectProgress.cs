39ffc141e2878ed730f580afd8c38569c8e5bd51
Miloš Kudělka <milos.kudelka@newtontech.cz>
2019-09-17 14:55:12 +0200

    Move IProjectProgress from Backend.

---
+++ Messaging/IProjectProgress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beey.DataExchangeModel.Messaging
+{
+    public interface IProjectProgress
+    {
+        int ProjectId { get; }
+        IEnumerable<Message> MessageHistory { get; }
+        ProcessState UploadState { get; }
+        ProcessState TranscodingState { get; }
+        ProcessState TranscriptionState { get; }
+        ProcessState DiarizationState { get; }
+
+        System.Threading.Tasks.Task StopAsync();
+    }
+    public enum ProcessState { None, Running, Completed, Failed }
+}
