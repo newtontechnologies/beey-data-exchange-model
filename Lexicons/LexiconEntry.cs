b2df217428a205317a28cf8c1f9629de0161a586
Miloš Kudělka <milos.kudelka@newtontech.cz>
2019-11-12 10:49:36 +0100

    Add LexiconEntry class for UserLex.

---
+++ Lexicons/LexiconEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beey.DataExchangeModel.Lexicons
+{
+    public class LexiconEntry
+    {
+        public string Text { get; set; }
+        public string Pronunciation { get; set; }
+        
+        public LexiconEntry(string text, string pronunciation)
+        {
+            Text = text;
+            Pronunciation = pronunciation;
+        }
+    }
+}
