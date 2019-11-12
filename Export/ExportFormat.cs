00ff5ff3c25c4727fd3fc0416b3394ed821310e3
Ladislav Seps <ladislav.seps@newtontech.cz>
2019-11-12 11:58:38 +0100

    Added export classes and examples

---
+++ Export/ExportFormat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beey.DataExchangeModel.Export
+{
+    public class ExportFormat
+    {
+        public string Id { get; set; }
+        public string Extension { get; set; }
+        public string Description { get;  set; }
+    }
+}
