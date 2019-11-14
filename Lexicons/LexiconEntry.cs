12f9e66fe7559e4a6abb7276acb989a1fb7ad2a3
Miloš Kudělka <milos.kudelka@newtontech.cz>
2019-11-14 15:41:29 +0100

    Make LexiconEntry properties required in Json.

--- Lexicons/LexiconEntry.cs
+++ Lexicons/LexiconEntry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,9 @@ namespace Beey.DataExchangeModel.Lexicons
 {
     public class LexiconEntry
     {
+        [JsonProperty(Required = Required.Always)]
         public string Text { get; set; }
+        [JsonProperty(Required = Required.Always)]
         public string Pronunciation { get; set; }
         
         public LexiconEntry(string text, string pronunciation)
