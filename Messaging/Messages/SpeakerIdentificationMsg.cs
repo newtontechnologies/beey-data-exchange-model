4bafbd7ecf7b5b5fdd8cad48410e4dd099771746
Ladislav Seps <ladislav.seps@newtontech.cz>
2019-11-19 18:50:00 +0100

    Added message for speaker identification subsystem

---
+++ Messaging/Messages/SpeakerIdentificationMsg.cs
@@ -0,0 +1,23 @@
+﻿using Beey.DataExchangeModel.Transcriptions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TranscriptionCore;
+
+#pragma warning disable nullable
+#pragma warning disable 8618
+namespace Beey.DataExchangeModel.Messaging.Messages
+{
+    public partial class SpeakerIdentificationMsg : Message<SpeakerIdentificationMsg.MessageKind>
+    {
+        public string XMLSpeaker { get; set; }
+        public enum MessageKind
+        {
+            Speaker,
+            Unidentified,
+        }
+    }
+}
