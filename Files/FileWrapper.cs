2894b52f8da93d953ed2c9ed9c1e106552dc5e36
Ladislav Seps <ladislav.seps@newtontech.cz>
2020-01-29 19:44:21 +0100

    added sanitized File name to wrapper

--- Files/FileWrapper.cs
+++ Files/FileWrapper.cs
@@ -1,5 +1,6 @@
 ﻿using Beey.DataExchangeModel.Serialization;
 using System;
+using System.IO;
 
 #pragma warning disable nullable
 #pragma warning disable 8618
@@ -13,9 +14,25 @@ namespace Beey.DataExchangeModel.Files
 
         }
 
+        /// <summary>
+        /// do not use for file names, can contain invalid characters
+        /// </summary>
         [JsonIgnoreWebDeserialize]
         public string Name { get; set; }
+
+
+        //TODO: this should be solved in some other way that will not expect that invalid GetInvalidFileNameChars does not change, and possibly prevent conflicts..
         [JsonIgnoreWebDeserialize]
+        public string ValidFileName => MakeValidFileName(Name);
+
+        static string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(Path.GetInvalidFileNameChars()));
+        static string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+        private static string MakeValidFileName(string name)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
+        }
+
+        [JsonIgnoreWeb]
         public long Size { get; set; }
     }
 }
