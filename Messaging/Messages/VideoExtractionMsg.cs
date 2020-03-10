bd270c0403c575fe5f1b1ef8d939dfc54db0b291
Ladislav Seps <ladislav.seps@newtontech.cz>
2020-03-11 00:54:43 +0100

    added video message

---
+++ Messaging/Messages/VideoExtractionMsg.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Messaging.Messages
+{
+    public partial class VideoExtractionMsg : Message<VideoExtractionMsg.MessageKind>
+    {
+        public enum MessageKind
+        {
+            Started,
+            Completed,
+            Failed,
+        }
+    }
+}
