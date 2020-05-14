845c6c8a2ee074b664746cb6b6478ef94d904ade
Miloš Kudělka <milos.kudelka@newtontech.cz>
2020-05-14 12:09:52 +0200

    Update SpeakerIdentificationData.

--- Messaging/Subsystems/SpeakerIdentificationData.cs
+++ Messaging/Subsystems/SpeakerIdentificationData.cs
@@ -8,11 +8,13 @@ namespace Beey.DataExchangeModel.Messaging.Subsystems
     class SpeakerIdentificationData : SubsystemData
     {
         public string XmlSpeaker { get; set; }
-        public ASRMsg CorrespondingAsrMsg { get; set; }
+        public int AsrMsgId { get; set; }
 
         public override void Initialize(JsonData data)
         {
-            throw new NotImplementedException();
+            AsrMsgId = data.JsonElement.GetProperty(nameof(AsrMsgId)).GetInt32();
+            if (data.JsonElement.TryGetProperty(nameof(XmlSpeaker), out var xmlSpeakerProp))
+                XmlSpeaker = xmlSpeakerProp.GetString();
         }
     }
 }
