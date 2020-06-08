65cb0fd239c81335faad36ff1e794f1d77e4c7ec
Miloš Kudělka <milos.kudelka@newtontech.cz>
2020-06-08 14:23:53 +0200

    Add WithVAD parameter to recognition config.

--- RecognitionConfig.cs
+++ RecognitionConfig.cs
@@ -4,17 +4,19 @@ namespace Beey.DataExchangeModel
 {
     public partial class RecognitionConfig
     {
-        public RecognitionConfig(bool saveTrsx, string language, bool withPPC, int userId)
+        public RecognitionConfig(bool saveTrsx, string language, bool withPPC, bool withVAD, int userId)
         {
             SaveTrsx = saveTrsx;
             Language = language;
             WithPPC = withPPC;
+            WithVAD = withVAD;
             UserID = userId;
         }
 
         public bool SaveTrsx { get; }
         public string Language { get; }
         public bool WithPPC { get; }
+        public bool WithVAD { get; }
         public int UserID { get; }
     }
 }
