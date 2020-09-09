76719c3c96e41af7184e90dc675c752bb77406ab
Miloš Kudělka <milos.kudelka@newtontech.cz>
2020-09-09 10:47:58 +0200

    Add voiceprint model info to config

--- Messaging/Subsystems/SpeakerIdentificationConfig.cs
+++ Messaging/Subsystems/SpeakerIdentificationConfig.cs
@@ -1,23 +1,38 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Beey.DataExchangeModel.Voiceprints;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 
 namespace Beey.DataExchangeModel.Messaging.Subsystems
 {
-    class SpeakerIdentificationConfig : SubsystemConfig<SpeakerIdentificationConfig>
+    public class SpeakerIdentificationConfig : SubsystemConfig<SpeakerIdentificationConfig>
     {
-        public string Language { get; internal set; }
-        public string VoiceprintVersion { get; internal set; }
-        public int SufficientSpeakerDuration { get; internal set; }
+        public string Language { get; set; }
+        public string VoiceprintVersion { get; set; }
+        public int SufficientSpeakerDuration { get; set; }
+        public VoiceprintModelInfo[] VoiceprintModels { get; set; }
 
         protected override void AddToConfiguration(IConfigurationBuilder builder)
         {
-            builder.AddInMemoryCollection(new Dictionary<string, string>()
+            var dict = new Dictionary<string, string>()
             {
                 { nameof(Language), Language },
                 { nameof(VoiceprintVersion), VoiceprintVersion },
                 { nameof(SufficientSpeakerDuration), SufficientSpeakerDuration.ToString() }
-            });
+            };
+
+            if (VoiceprintModels != null)
+            {
+                //dict.Add($"{nameof(VoiceprintModels)}", "");
+                for (int i = 0; i < VoiceprintModels.Length; i++)
+                {
+                    //dict.Add($"{nameof(VoiceprintModels)}:{i}", "");
+                    dict.Add($"{nameof(VoiceprintModels)}:{i}:{nameof(VoiceprintModelInfo.Hash)}", VoiceprintModels[i].Hash);
+                    dict.Add($"{nameof(VoiceprintModels)}:{i}:{nameof(VoiceprintModelInfo.AcceptanceThreshold)}", VoiceprintModels[i].AcceptanceThreshold.ToString());
+                }
+            }
+
+            builder.AddInMemoryCollection(dict);
         }
     }
 }
--- Messaging/Subsystems/SpeakerIdentificationData.cs
+++ Messaging/Subsystems/SpeakerIdentificationData.cs
@@ -9,7 +9,7 @@ namespace Beey.DataExchangeModel.Messaging.Subsystems
     {
         public string XmlSpeaker { get; set; }
         public int DiarizationMsgId { get; set; }
-        public float SpeakerScore { get; set; }
-        public float[] SpeakerVector { get; set; }
+        public float? Score { get; set; }
+        public string Voiceprint { get; set; }
     }
 }
