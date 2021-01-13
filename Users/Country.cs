881d7bcae38d94f326b6a9087934f284dd6940aa
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-01-13 15:56:25 +0100

    Add Country class

---
+++ Users/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Users
+{
+    public class Country
+    {
+        public string NativeName { get; set; }
+        public string Code { get; set; }
+
+        public Country(string nativeName, string code)
+        {
+            NativeName = nativeName;
+            Code = code;
+        }
+    }
+}
