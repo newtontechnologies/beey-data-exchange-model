aba536d367d79ece4e99fd3204f8c75b3583b94c
sobeslavbenda <sobeslav.benda@newtontech.cz>
2021-03-05 13:40:29 +0100

    Add search request and response

---
+++ Projects/SearchRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beey.DataExchangeModel.Projects
+{
+    public class SearchRequest
+    {
+        public int From { set; get; }
+
+        public int Size { set; get; }
+
+        public string Sort { set; get; }
+
+        public string Order { set; get; }
+
+        public string ProjectNameFullText { set; get; }
+
+        public string TranscriptionFullText { set; get; }
+
+        public DateTime? CreatedFrom { set; get; }
+
+        public DateTime? CreatedTo { set; get; }
+
+        public DateTime? UpdatedFrom { set; get; }
+
+        public DateTime? UpdatedTo { set; get; }
+
+        public double? DurationFrom { set; get; }
+
+        public double? DurationTo { set; get; }
+
+        public bool? MediaHasVideo { set; get; }
+
+
+        public SearchRequest()
+        {
+            Size = 10;
+        }
+
+    }
+}
---
+++ Projects/SearchResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beey.DataExchangeModel.Projects
+{
+    public class SearchResponse
+    {
+        public SearchResult[] Results { set; get; }
+    }
+
+    public class SearchResult
+    {
+        public int ProjectId { set; get; }
+
+        public string[] Name { set; get; }
+
+        public string[] Text { set; get; }
+    }
+}
