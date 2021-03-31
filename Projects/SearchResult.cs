eecd0a3f47b4993d2b5cccbe6ce432bddf81f324
sobeslavbenda <sobeslav.benda@newtontech.cz>
2021-03-31 15:57:59 +0200

    Add props to SearchResult

--- Projects/SearchResult.cs
+++ Projects/SearchResult.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Beey.DataExchangeModel.Projects
-{
-    public class SearchResponse
-    {
-        public int Total { set; get; }
-
-        public SearchResult[] Results { set; get; }
-    }
-
+{   
     public class SearchResult
     {
         public int ProjectId { set; get; }
-       
+
+        public string ProjectName { set; get; }
+
+        public string[] Tags { set; get; }
+
+        public TimeSpan Length { get; set; }
+
+        public DateTime? Created { set; get; }
+
+        public DateTime? Updated { set; get; }
+
         public string[] ProjectNameHighlight { set; get; }
 
         public string[] TranscriptionHighlight { set; get; }
