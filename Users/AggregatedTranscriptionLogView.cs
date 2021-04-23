7c82c986ac1345925bdde19f4cdbce88bbc2325e
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-04-23 15:32:05 +0200

    Add class for aggregated transcription log

---
+++ Users/AggregatedTranscriptionLogView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Users
+{
+    public class AggregatedTranscriptionLogView
+    {
+        public int Year { get; }
+        public int Month { get; }
+        public decimal Minutes { get; }
+
+        public AggregatedTranscriptionLogView(int year, int month, decimal minutes)
+        {
+            Year = year;
+            Month = month;
+            Minutes = minutes;
+        }
+    }
+}
