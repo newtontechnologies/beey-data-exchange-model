464dbdbf4b38b882df161c1014d0ec1813654b81
Ladislav Seps <ladislav.seps@newtontech.cz>
2021-05-07 18:10:01 +0200

    fix: transcription trial serialization

--- Messaging/Subsystems/TranscriptionConfig.cs
+++ Messaging/Subsystems/TranscriptionConfig.cs
@@ -38,6 +38,7 @@ namespace Beey.DataExchangeModel.Messaging.Subsystems
                 { nameof(WithVAD), WithVAD.ToString() },
                 { nameof(UserId), UserId.ToString() },
                 { nameof(WithPunctuation), WithPunctuation.ToString() },
+                { nameof(TrialTranscription), TrialTranscription.ToString() },
             });
         }
     }
