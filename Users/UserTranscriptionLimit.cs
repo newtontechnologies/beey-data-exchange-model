0efa47b7f2eebb06b067c85cee1e5ac37485cc00
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-05-31 15:08:59 +0200

    Add UserTranscriptionLimit

---
+++ Users/UserTranscriptionLimit.cs
@@ -0,0 +1,16 @@
+﻿using Beey.DataExchangeModel.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Users
+{
+    public class UserTranscriptionLimit : EntityBase
+    {
+        public int UserId { get; set; }
+        public QueueType QueueType { get; set; }
+        public int Limit { get; set; }
+    }
+}
