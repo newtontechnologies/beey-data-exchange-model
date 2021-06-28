ee712b84e0069984ccd2c72de9fcf5b045cb9fa5
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-06-28 17:01:00 +0200

    Add Project state

---
+++ Projects/ProjectErrorCause.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Projects
+{
+    public enum ProjectErrorCause
+    {
+        None,
+        Precondition,
+        Upload,
+        Transcoding,
+        Transcription,
+        Unknown
+    }
+
+    public enum ProjectErrorCauseDetail_Precondition
+    {
+        None = 0,
+        NotEnoughCredit = 1,
+    }
+    public enum ProjectErrorCauseDetail_Transcoding
+    {
+        None = 0,
+        
+    }
+}
---
+++ Projects/ProjectProcessingState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Projects
+{
+    public enum ProjectProcessingState
+    {
+        /// <summary>
+        /// Project is empty - after creation.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Project is being transcoded.
+        /// </summary>
+        TranscodingOnly,
+
+        /// <summary>
+        /// Project is in a queue and will be transcribed. Transcoding is running or already finished.
+        /// </summary>
+        Queued,
+
+        /// <summary>
+        /// Project is waiting for media to be ready to start transcription.
+        /// </summary>
+        WaitingForTranscription,
+
+        /// <summary>
+        /// Project is being transcribed.
+        /// </summary>
+        Transcribing,
+
+        /// <summary>
+        /// Project processing was canceled by user.
+        /// </summary>
+        Canceled,
+
+        /// <summary>
+        /// Project was processed successfuly.
+        /// </summary>
+        Completed,
+
+        /// <summary>
+        /// There was an error during processing of the project.
+        /// </summary>
+        Failed
+    }
+}
