f389cfe0bf24a3ea7768778d87602465206c9ffc
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-08-10 16:05:39 +0200

    Add workspace class and add WorkspaceId to User

---
+++ Workspaces/Workspace.cs
@@ -0,0 +1,15 @@
+﻿using Beey.DataExchangeModel.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Workspaces
+{
+    public class Workspace : EntityBase
+    {
+        public int OwnerId { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
