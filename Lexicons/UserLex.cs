0bdefe1afae84640cb742cae330cc4c5ca27b08d
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-08-11 15:19:06 +0200

    Move UserLex to workspace

--- Lexicons/UserLex.cs
+++ Lexicons/UserLex.cs
@@ -6,7 +6,7 @@ namespace Beey.DataExchangeModel.Lexicons
 {
     public class UserLex : EntityBase
     {
-        public int UserId { get; set; }
+        public int WorkspaceId { get; set; }
         public string Language { get; set; }
         public string Lexicon { get; set; }
     }
