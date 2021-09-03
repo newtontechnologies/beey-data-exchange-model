f2bae64c6bfe258387f79eb731d4de5d31fcb2bc
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-09-03 14:51:39 +0200

    Add Undefinable struct and Json converter for it

---
+++ Serialization/JsonConverters/JsonUndefinableConverter.cs
@@ -0,0 +1,40 @@
+﻿using Beey.DataExchangeModel.Tools;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Serialization.JsonConverters
+{
+    public class JsonUndefinableConverter : JsonConverter
+    {
+        private static readonly Type TypeOfIUndefinable = typeof(IUndefinable);
+
+        public override bool CanConvert(Type objectType) => TypeOfIUndefinable.IsAssignableFrom(objectType);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (existingValue == null)
+                existingValue = Activator.CreateInstance(objectType);
+
+            if (reader.TokenType != JsonToken.Undefined)
+            {
+                var undefinable = (IUndefinable)existingValue;
+                undefinable.Value = serializer.Deserialize(reader, undefinable.ValueType);
+            }
+
+            return existingValue;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var undefinable = (IUndefinable)value;
+            if (!undefinable.IsDefined)
+                writer.WriteUndefined();
+            else
+                serializer.Serialize(writer, undefinable.Value);
+        }
+    }
+}
---
+++ Tools/Undefinable.cs
@@ -0,0 +1,74 @@
+﻿using Beey.DataExchangeModel.Serialization.JsonConverters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Tools
+{
+    /// <summary>
+    /// For easier handling in custom json converters.
+    /// </summary>
+    public interface IUndefinable
+    {
+        object Value { get; set; }
+        Type ValueType { get; }
+        bool IsDefined { get; }
+    }
+
+    [JsonConverter(typeof(JsonUndefinableConverter))]
+    public struct Undefinable<T> : IUndefinable
+    {
+        private static readonly Type ValueType = typeof(T);
+        private bool isDefined;
+        private T value;
+
+        public bool IsDefined
+        {
+            get => isDefined;
+            set
+            {
+                isDefined = value;
+                if (!value)
+                    Value = default;
+            }
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (!isDefined)
+                    throw new InvalidOperationException("Undefinable value is undefined.");
+                return value;
+            }
+            set
+            {
+                this.value = value;
+                isDefined = true;
+            }
+        }
+
+        public Undefinable(T value)
+        {
+            this.value = value;
+            this.isDefined = true;
+        }
+
+        object IUndefinable.Value { get => Value; set => Value = (T)value; }
+        Type IUndefinable.ValueType => ValueType;
+
+        public static implicit operator Undefinable<T>(T value) => new Undefinable<T>(value);
+
+        public static explicit operator T(Undefinable<T> undefinable) => undefinable.Value;
+
+        public override string ToString()
+        {
+            if (!IsDefined || Value == null)
+                return "";
+            return Value.ToString();
+        }
+    }
+}
