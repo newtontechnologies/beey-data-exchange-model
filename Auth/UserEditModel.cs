6c232cb7d3f2ec4fb3f25a1997cfb32445aadf4e
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-09-06 17:08:21 +0200

    Merge partial class User to backend part, add UserViewModel and UserEditModel and use them on API instead of DB model class User

---
+++ Auth/UserEditModel.cs
@@ -0,0 +1,19 @@
+﻿using Beey.DataExchangeModel.Tools;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Beey.DataExchangeModel.Auth
+{
+    public class UserEditModel
+    {
+        [Obsolete("For backwards compatibility only. Is part of workspaces now.")]
+        public Undefinable<int> CreditMinutes { get; set; }
+        public Undefinable<string> Email { get; set; }
+        public Undefinable<int> Id { get; set; }
+        public Undefinable<string> Language { get; set; }
+        public Undefinable<JObject> Settings { get; set; }
+        public Undefinable<UserRole> UserRole { get; set; }
+        public Undefinable<string> Password { get; set; }
+        public Undefinable<int> WorkspaceId { get; set; }
+    }
+}
---
+++ Auth/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Beey.DataExchangeModel.Auth
+{
+    public class UserViewModel
+    {
+        public bool CommercialCommunicationConsent { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public int CreditMinutes { get; set; }
+        public bool DataProtectionConsent { get; set; }
+        public bool DidPay { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+        public string Language { get; set; }
+        public JObject Settings { get; set; }
+        public decimal TranscribedMinutes { get; set; }
+        public DateTimeOffset Updated { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public UserRole UserRole { get; set; }
+        public int WorkspaceId { get; set; }
+        public bool IsWorkspaceOwner { get; set; }
+    }
+}
