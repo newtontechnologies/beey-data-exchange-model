5e8a04fe4bbd2086c7a746b01b86c1ce7aee77f1
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-09-07 13:46:32 +0200

    Add WorkspaceViewModel

---
+++ Workspaces/WorkspaceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Beey.DataExchangeModel.Auth;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beey.DataExchangeModel.Workspaces
+{
+    public class WorkspaceViewModel
+    {
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public UserViewModel[] Members { get; set; }
+        public int CreditMinutes { get; set; }
+        public decimal TranscribedMinutes { get; set; }
+        public bool DidPay { get; set; }
+        public JArray CustomProjectTags { get; set; }
+    }
+}
