fb14b75d53d70f9fd63edf0be75b484ff5071d88
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-09-14 13:15:35 +0200

    Extend WorkspaceViewModel according to specification

--- Workspaces/WorkspaceViewModel.cs
+++ Workspaces/WorkspaceViewModel.cs
@@ -12,10 +12,34 @@ namespace Beey.DataExchangeModel.Workspaces
     {
         public int Id { get; set; }
         public int OwnerId { get; set; }
-        public (int Id, string Email)[] Members { get; set; }
         public int CreditMinutes { get; set; }
         public decimal TranscribedMinutes { get; set; }
         public bool DidPay { get; set; }
         public JArray CustomProjectTags { get; set; }
+    }
+
+    public class WorkspaceStandardViewModel : WorkspaceViewModel
+    {
+        public class Member
+        {
+            public int Id { get; set; }
+            public string Email { get; set; }
+        }
+
+        public IEnumerable<Member> Members { get; set; }
+    }
+
+    public class WorkspaceAdminViewModel : WorkspaceViewModel
+    {
+        public class Member
+        {
+            public int Id { get; set; }
+            public string Email { get; set; }
+            public decimal? Transcribed { get; set; }
+            public DateTime? From { get; set; }
+            public DateTime? To { get; set; }
+        }
+
+        public IEnumerable<Member> Members { get; set; }
     }
 }
