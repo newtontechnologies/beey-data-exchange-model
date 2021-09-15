6599f5523f3121ff432f3839ba2b3f449dc2f3f8
Miloš Kudělka <milos.kudelka@newtontech.cz>
2021-09-15 15:38:40 +0200

    Replace OwnerId with Owner property in Workspace view model

--- Workspaces/WorkspaceViewModel.cs
+++ Workspaces/WorkspaceViewModel.cs
@@ -11,7 +11,6 @@ namespace Beey.DataExchangeModel.Workspaces
     public class WorkspaceViewModel
     {
         public int Id { get; set; }
-        public int OwnerId { get; set; }
         public int CreditMinutes { get; set; }
         public decimal TranscribedMinutes { get; set; }
         public bool DidPay { get; set; }
@@ -28,6 +27,7 @@ namespace Beey.DataExchangeModel.Workspaces
             public string Email { get; set; }
         }
 
+        public Member Owner { get; set; }
         public IEnumerable<Member> Members { get; set; }
     }
 
@@ -42,6 +42,7 @@ namespace Beey.DataExchangeModel.Workspaces
             public DateTime? To { get; set; }
         }
 
+        public Member Owner { get; set; }
         public IEnumerable<Member> Members { get; set; }
     }
 }
