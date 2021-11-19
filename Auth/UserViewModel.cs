c01377bfe53c8b86794e803ee162902467bcfe31
Ladislav Seps <ladislav.seps@newtontech.cz>
2021-11-19 11:44:49 +0100

    fix:Teams crash on old database

--- Auth/UserViewModel.cs
+++ Auth/UserViewModel.cs
@@ -8,7 +8,7 @@ namespace Beey.DataExchangeModel.Auth
     public class UserViewModel
     {
         public bool CommercialCommunicationConsent { get; set; }
-        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Created { get; set; }
         public int CreditMinutes { get; set; }
         public bool DataProtectionConsent { get; set; }
         public bool DidPay { get; set; }
@@ -17,7 +17,7 @@ namespace Beey.DataExchangeModel.Auth
         public string Language { get; set; }
         public JObject Settings { get; set; }
         public decimal TranscribedMinutes { get; set; }
-        public DateTimeOffset Updated { get; set; }
+        public DateTimeOffset? Updated { get; set; }
         [JsonConverter(typeof(StringEnumConverter))]
         public UserRole UserRole { get; set; }
         public int TeamId { get; set; }
