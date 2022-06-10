b3c7ec07b6e65ab1b89ff7f4c0e2555fe9830ecd
Ladislav Šeps <ladislav.seps@newtontech.cz>
2022-06-10 17:28:51 +0200

    fix: transcription config constructor param order same as master

--- Beey.DataExchangeModel.Partial/Messaging/Subsystems/TranscriptionConfig.cs
+++ Beey.DataExchangeModel.Partial/Messaging/Subsystems/TranscriptionConfig.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 namespace Beey.DataExchangeModel.Messaging.Subsystems
 {
-    public record TranscriptionConfig(bool SaveTrsx, string Language, bool WithPPC, bool WithVAD, bool WithPunctuation, int UserId, bool TrialTranscription, string Profile, bool WithSpeakerId, bool WithDiarization)
+    public record TranscriptionConfig(bool SaveTrsx, string Language, string Profile, bool WithPPC, bool WithVAD, bool WithPunctuation, int UserId, bool TrialTranscription, bool WithSpeakerId, bool WithDiarization)
     {
 
     }
