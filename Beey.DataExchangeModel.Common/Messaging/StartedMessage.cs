12805a350249364e58e9bf1095fe80ce315eed62
Ladislav Šeps <ladislav.seps@newtontech.cz>
2022-07-14 15:01:33 +0200

    fix: compatibility with old deserialization of config on started messages

--- Beey.DataExchangeModel.Common/Messaging/StartedMessage.cs
+++ Beey.DataExchangeModel.Common/Messaging/StartedMessage.cs
@@ -2,10 +2,14 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Immutable;
+using System.Text.Json.Nodes;
 
 namespace Beey.DataExchangeModel.Messaging;
 
 public abstract record StartedMessage(int Id, ImmutableArray<int> Index, int? ProjectId, string Subsystem, DateTimeOffset Sent) : Message(Id, Index, ProjectId, Subsystem, Sent)
 {
     public override MessageType Type => MessageType.Started;
+
+    [Obsolete("just for backwards compatibility with deserializers")]
+    public JsonNode Config => new JsonObject();
 }
