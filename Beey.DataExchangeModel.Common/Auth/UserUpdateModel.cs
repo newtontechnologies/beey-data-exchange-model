cfcb3c33cfe94cd52e0fee0147496dff01388dd3
Ladislav Šeps <ladislav.seps@newtontech.cz>
2022-07-14 11:57:45 +0200

    FIX: for backwards compatibility FE sends UserUpdateModel.Id as json string not json number

--- Beey.DataExchangeModel.Common/Auth/UserUpdateModel.cs
+++ Beey.DataExchangeModel.Common/Auth/UserUpdateModel.cs
@@ -1,11 +1,13 @@
 ﻿using Beey.DataExchangeModel.Tools;
 using System;
 using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace Beey.DataExchangeModel.Auth;
 
 public class UserUpdateModel
 {
+    [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
     public int Id { get; set; }
     [Obsolete("For backwards compatibility only. Is part of teams now.")]
     public Undefinable<int> CreditMinutes { get; set; }
