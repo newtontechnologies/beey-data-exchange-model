8437c083793d0821e0f9b1824bce1ad053d5c652
Miloš Kudělka <milos.kudelka@newtontech.cz>
2022-09-22 15:28:29 +0200

    Set withSpeakerId only when also withDiarization is set in transcription config

--- Beey.DataExchangeModel.Common/Messaging/Subsystems/TranscriptionConfig.cs
+++ Beey.DataExchangeModel.Common/Messaging/Subsystems/TranscriptionConfig.cs
@@ -6,7 +6,30 @@ using System.Collections.Generic;
 
 namespace Beey.DataExchangeModel.Messaging.Subsystems;
 
-public record TranscriptionConfig(bool SaveTrsx, string Language, string Profile, bool WithPPC, bool WithVAD, bool WithPunctuation, int UserId, bool TrialTranscription, bool WithSpeakerId, bool WithDiarization)
+public record TranscriptionConfig
 {
+    public TranscriptionConfig(bool saveTrsx, string language, string profile, bool withPPC, bool withVAD, bool withPunctuation, int userId, bool trialTranscription, bool withSpeakerId, bool withDiarization)
+    {
+        SaveTrsx = saveTrsx;
+        Language = language;
+        Profile = profile;
+        WithPPC = withPPC;
+        WithVAD = withVAD;
+        WithPunctuation = withPunctuation;
+        UserId = userId;
+        TrialTranscription = trialTranscription;
+        WithSpeakerId = withDiarization && withSpeakerId;
+        WithDiarization = withDiarization;
+    }
 
+    public bool SaveTrsx { get; }
+    public string Language { get; }
+    public string Profile { get; }
+    public bool WithPPC { get; }
+    public bool WithVAD { get; }
+    public bool WithPunctuation { get; }
+    public int UserId { get; }
+    public bool TrialTranscription { get; }
+    public bool WithSpeakerId { get; }
+    public bool WithDiarization { get; }
 }
