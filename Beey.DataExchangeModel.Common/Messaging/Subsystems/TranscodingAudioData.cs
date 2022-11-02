5c60648f44af0d2105ab22c2befa9db0fa7fdbec
Ladislav Šeps <ladislav.seps@newtontech.cz>
2022-11-02 15:34:25 +0100

    added Transcoding audio/video data

---
+++ Beey.DataExchangeModel.Common/Messaging/Subsystems/TranscodingAudioData.cs
@@ -0,0 +1,17 @@
+﻿using Beey.DataExchangeModel.Projects;
+using Beey.DataExchangeModel.Serialization.JsonConverters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Beey.DataExchangeModel.Messaging.Subsystems;
+
+public class TranscodingAudioData : SubsystemData<TranscodingAudioData>
+{
+    public string Record { get; init; } = default!;
+
+}
