258f0d5d183baaa3801e4c91499df84232b0f82b
Miloš Kudělka <milos.kudelka@newtontech.cz>
2022-11-21 16:01:48 +0100

    Set non-required fields for PaymentInfo to nulalble

--- Beey.DataExchangeModel.Common/Users/PaymentInfoViewModel.cs
+++ Beey.DataExchangeModel.Common/Users/PaymentInfoViewModel.cs
@@ -9,28 +9,28 @@ namespace Beey.DataExchangeModel.Users;
 
 public class PaymentInfoAddModel
 {
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Company { get; set; }
+    public string Email { get; set; } = default!;
+    public string? PhoneNumber { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string? Company { get; set; }
     /// <summary>
     /// DIČ
     /// </summary>
-    public string TaxIdentificationNumber { get; set; }
+    public string? TaxIdentificationNumber { get; set; }
     /// <summary>
     /// IČO
     /// </summary>
-    public string CompanyIdentificationNumber { get; set; }
+    public string? CompanyIdentificationNumber { get; set; }
     //public string VATIdentificationNumber { get; set; } //in USA TAX and VAT ids are different
 
-    public string Address { get; set; }
-    public string AddressComplement { get; set; }
-    public string PostalCode { get; set; }
-    public string City { get; set; }
-    public string Country { get; set; }
-    public string CountryCode { get; set; }
-    public string StateOrRegion { get; set; }
+    public string Address { get; set; } = default!;
+    public string? AddressComplement { get; set; }
+    public string PostalCode { get; set; } = default!;
+    public string City { get; set; } = default!;
+    public string Country { get; set; } = default!;
+    public string CountryCode { get; set; } = default!;
+    public string? StateOrRegion { get; set; }
 
     public bool ReverseCharge { get; set; }
 }
