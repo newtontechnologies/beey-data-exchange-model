9328e529d8644a12f680387b53ee788fee8940e9
Richard Klapáč <richard.klapac@newtontech.cz>
2024-01-24 22:26:09 +0100

    add more transcode errors, wip ffmpeg exit codes

--- Beey.DataExchangeModel.Common/Project/ProjectErrorCategory.cs
+++ Beey.DataExchangeModel.Common/Project/ProjectErrorCategory.cs
@@ -28,6 +28,8 @@ public enum ProjectErrorReason_Transcoding
     NoAudioProduced,
     NoVideoProduced,
     TranscodingStuck,
+    UnsupportedMedia,
+    CorruptedMedia,
 }
 
 public enum ProjectErrorReason_Transcription
@@ -43,4 +45,17 @@ public enum ProjectErrorReason_General
     Unknown,
     NotEnoughCredit,
     ServerShutdown,
+}
+
+// https://ffmpeg.org//doxygen/trunk/error_8h_source.html
+// codes are reversed, converted into their ascii counterparts and then to decimal
+// 
+// TODO move away
+public enum FFmpegExitCodes
+{
+    DecoderNotFound = -1128613112,
+    DemuxerNotFound = -1296385272,
+    EncoderNotFound = -1129203192,
+    MuxerNotFound = -1481985528,
+    InvalidDataFound = -1094995529,
 }
