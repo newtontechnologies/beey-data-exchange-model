51ce7107a2fca6127f283661a989dc6080841f80
Richard Klapáč <richard.klapac@newtontech.cz>
2024-01-30 15:50:54 +0100

    remove forgotten ffmpegstatuscodes from previous file

--- Beey.DataExchangeModel.Common/Project/ProjectErrorCategory.cs
+++ Beey.DataExchangeModel.Common/Project/ProjectErrorCategory.cs
@@ -49,16 +49,3 @@ public enum ProjectErrorReason_General
     NotEnoughCredit,
     ServerShutdown,
 }
-
-// https://ffmpeg.org//doxygen/trunk/error_8h_source.html
-// codes are reversed, converted into their ascii counterparts and then to decimal
-// 
-// TODO move away
-public enum FFmpegExitCodes
-{
-    DecoderNotFound = -1128613112,
-    DemuxerNotFound = -1296385272,
-    EncoderNotFound = -1129203192,
-    MuxerNotFound = -1481985528,
-    InvalidDataFound = -1094995529,
-}
