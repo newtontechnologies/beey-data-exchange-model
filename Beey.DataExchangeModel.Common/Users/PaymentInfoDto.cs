7cd10e84538bf00aa76662b0ccd05a7a20b4c04a
Jitka H:)bnerová <jitka.darbujanova@gmail.com>
2024-02-13 14:10:18 +0100

    payment info non required fields

--- Beey.DataExchangeModel.Common/Users/PaymentInfoDto.cs
+++ Beey.DataExchangeModel.Common/Users/PaymentInfoDto.cs
@@ -11,8 +11,8 @@ public class PaymentInfoAddDto
 {
     public string Email { get; set; } = default!;
     public string? PhoneNumber { get; set; }
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
+    public string? FirstName { get; set; } = default!;
+    public string? LastName { get; set; } = default!;
     public string? Company { get; set; }
     /// <summary>
     /// DIČ
@@ -24,10 +24,10 @@ public class PaymentInfoAddDto
     public string? CompanyIdentificationNumber { get; set; }
     //public string VATIdentificationNumber { get; set; } //in USA TAX and VAT ids are different
 
-    public string Address { get; set; } = default!;
+    public string? Address { get; set; } = default!;
     public string? AddressComplement { get; set; }
-    public string PostalCode { get; set; } = default!;
-    public string City { get; set; } = default!;
+    public string? PostalCode { get; set; } = default!;
+    public string? City { get; set; } = default!;
     public string Country { get; set; } = default!;
     public string CountryCode { get; set; } = default!;
     public string? StateOrRegion { get; set; }
