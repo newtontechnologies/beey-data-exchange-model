b177ce8f21fe0e8a6502434408a1be117672acc7
Petr Bříza <petr.briza@gmail.com>
2024-05-15 09:38:55 +0200

    Move SpeakerCatalogScope to common API

---
+++ Beey.DataExchangeModel.Common/Speakers/SpeakerCatalogScope.cs
@@ -0,0 +1,7 @@
+﻿namespace Beey.DataExchangeModel.Common.Speakers;
+
+/// <summary> Scope in which a speaker is valid </summary>
+public class SpeakerCatalogScope
+{
+    public required string TenantId { get; init; }
+}
