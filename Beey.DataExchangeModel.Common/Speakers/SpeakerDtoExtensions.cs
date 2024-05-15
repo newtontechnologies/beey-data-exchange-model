c6cf4658efdc27a7f68055097c50ef4a04e058ec
Petr Bříza <petr.briza@gmail.com>
2024-05-15 09:01:08 +0200

    DTO for SpeakerCatalog service

---
+++ Beey.DataExchangeModel.Common/Speakers/SpeakerDtoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Beey.DataExchangeModel.Common.Speakers;
+public static class SpeakerDtoExtensions
+{
+    public static string? GetString(this SpeakerDto dto, SpeakerField field)
+        => dto.Data.TryGetPropertyValue(field.FieldName, out var propertyNode)
+           && propertyNode is JsonValue propertyValue
+           && propertyValue.TryGetValue<string>(out var value)
+            ? value
+            : null;
+
+    public static void SetString(this SpeakerDto dto, SpeakerField field, string? newValue)
+    {
+        if (newValue == null)
+            dto.Data.Remove(field.FieldName);
+        else
+            dto.Data[field.FieldName] = JsonValue.Create(newValue);
+    }
+}
---
+++ Beey.DataExchangeModel.Common/Speakers/SpeakerField.cs
@@ -0,0 +1,22 @@
+﻿namespace Beey.DataExchangeModel.Common.Speakers;
+
+/// <summary> Common well-known speaker field </summary>
+public readonly struct SpeakerField(string fieldName)
+{
+    public string FieldName { get; } = fieldName;
+    public override string ToString() => this.FieldName;
+
+    public static SpeakerField FirstName { get; } = new("firstName");
+
+    public static SpeakerField MiddleName { get; } = new("middleName");
+
+    public static SpeakerField LastName { get; } = new("lastName");
+
+    public static SpeakerField DegreeBefore { get; } = new("degreeBefore");
+
+    public static SpeakerField DegreeAfter { get; } = new("degreeAfter");
+
+    public static SpeakerField ImageBase64 { get; } = new("imageBase64");
+
+    public static SpeakerField DefaultIsoLang { get; } = new("defaultIsoLang");
+}
