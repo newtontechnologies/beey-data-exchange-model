c239761dbcd122f1d6b29d8c9ebcfb00089cfbcf
Petr Bříza <petr.briza@gmail.com>
2024-05-17 16:23:17 +0200

    Extract class CommonSpeakerFields

--- Beey.DataExchangeModel.Common/Speakers/SpeakerDtoExtensions.cs
+++ Beey.DataExchangeModel.Common/Speakers/SpeakerDtoExtensions.cs
@@ -3,14 +3,14 @@
 namespace Beey.DataExchangeModel.Common.Speakers;
 public static class SpeakerDtoExtensions
 {
-    public static string? GetString(this SpeakerDto dto, SpeakerField field)
+    public static string? Get(this SpeakerDto dto, SpeakerStringField field)
         => dto.Data.TryGetPropertyValue(field.FieldName, out var propertyNode)
            && propertyNode is JsonValue propertyValue
            && propertyValue.TryGetValue<string>(out var value)
             ? value
             : null;
 
-    public static void SetString(this SpeakerDto dto, SpeakerField field, string? newValue)
+    public static void Set(this SpeakerDto dto, SpeakerStringField field, string? newValue)
     {
         if (newValue == null)
             dto.Data.Remove(field.FieldName);
