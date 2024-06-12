636acb751c74025364741b49c8110d4873fee4ce
Petr Bříza <petr.briza@gmail.com>
2024-06-12 13:16:37 +0200

    ParamsProjectInit.Start field

--- Beey.DataExchangeModel.Common/Project/ParamsProjectInit.cs
+++ Beey.DataExchangeModel.Common/Project/ParamsProjectInit.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-
-
-namespace Beey.DataExchangeModel.Projects;
+﻿namespace Beey.DataExchangeModel.Projects;
 
 public class ParamsProjectInit
 {
-    public string Name { get; set; }
-    public string CustomPath { get; set; }
+    public string? Name { get; set; }
+    public string? CustomPath { get; set; }
+    public DateTimeOffset? Start { get; set; }
 }
