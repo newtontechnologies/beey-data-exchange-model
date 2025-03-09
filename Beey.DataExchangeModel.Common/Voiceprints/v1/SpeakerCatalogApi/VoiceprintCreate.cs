bcf1c67fc741cca2de78bf36bcd5c08ac98a0ecb
petr-briza <petr.briza@gmail.com>
2025-03-09 17:08:59 +0100

    SpeakerCatalog voiceprint creation and deletion

---
+++ Beey.DataExchangeModel.Common/Voiceprints/v1/SpeakerCatalogApi/VoiceprintCreate.cs
@@ -0,0 +1,24 @@
+﻿namespace Beey.DataExchangeModel.Common.Voiceprints.v1.SpeakerCatalogApi;
+
+public class VoiceprintCreateRequest
+{
+    public required string SpeakerId { get; init; }
+
+    /// <summary>IETF tag or null if language is unknown</summary>
+    /// <remarks>
+    /// This should be set if catalog is in model-per-language mode,
+    /// otherwise this voiceprint might not be used at all.
+    /// </remarks>
+    public required string? Language { get; set; }
+
+    public required string ProfileId { get; init; }
+
+    /// <summary>Voice-print as binary data</summary>
+    public required byte[] Data { get; init; }
+}
+
+public class VoiceprintCreateResponse
+{
+    /// <summary>Id of created voiceprint</summary>
+    public required int Id { get; init; }
+}
---
+++ Beey.DataExchangeModel.Common/Voiceprints/v1/SpeakerCatalogApi/VoiceprintDelete.cs
@@ -0,0 +1,10 @@
+﻿namespace Beey.DataExchangeModel.Common.Voiceprints.v1.SpeakerCatalogApi;
+
+public class VoiceprintDeleteRequest
+{
+    public string? TenantId { get; set; } // query param
+}
+
+public class VoiceprintDeleteResponse
+{
+}
