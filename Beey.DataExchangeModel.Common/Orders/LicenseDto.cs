f64dabe5b35c39426aa59569fac6278c910c2f02
Jitka H:)bnerova <Jitka.Hubnerova@newtontech.cz>
2025-05-19 16:57:43 +0200

    feat(vyv-3282) Add license dto to data exchange model

---
+++ Beey.DataExchangeModel.Common/Orders/LicenseDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Beey.DataExchangeModel.Common.Orders
+{
+    public record LicenseDto(
+        string Name,
+        DateTimeOffset TermStartDate,
+        DateTimeOffset TermEndDate,
+        string Email,
+        int TeamId,
+        string? Description,
+        bool IsActive,
+        int Quantity,
+        string? ComponentsJson,
+        string? LicenseFileBase64,
+        DateTimeOffset? LicenseGeneratedAt,
+        int? SubscriptionId,
+        string? SubscriptionKey
+    );
+
+}
