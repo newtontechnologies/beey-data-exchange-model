fd5a06d44ae973b41dcb8d607c22530ebb9a4dab
Jitka H:)bnerova <Jitka.Hubnerova@newtontech.cz>
2025-06-02 13:17:37 +0200

    feat(vyv-3282):add LicenseFileDto

---
+++ Beey.DataExchangeModel.Common/Orders/LicenseFileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Beey.DataExchangeModel.Common.Orders
+{
+    public record LicenseFileDto(
+        bool? IsActive,
+        string? LicenseFileBase64,
+        DateTimeOffset? LicenseGeneratedAt,
+        DateTimeOffset? LicenseDeactivatedAt
+    );
+}
