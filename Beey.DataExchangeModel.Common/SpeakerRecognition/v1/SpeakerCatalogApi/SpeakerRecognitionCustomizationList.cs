020b25ee4edb1cff90e6f114ed779e1ddb6abec8
petr-briza <petr.briza@gmail.com>
2025-07-20 21:20:16 +0200

    SpeakerCatalog customizations

---
+++ Beey.DataExchangeModel.Common/SpeakerRecognition/v1/SpeakerCatalogApi/SpeakerRecognitionCustomizationList.cs
@@ -0,0 +1,21 @@
+﻿namespace Beey.DataExchangeModel.Common.SpeakerRecognition.v1.SpeakerCatalogApi;
+
+public class SpeakerRecognitionCustomizationListRequest
+{
+    /// <summary>Find customizations for this language, or for all if this is null</summary>
+    public string? Language { get; set; } = null;
+
+    /// <summary>Find customizations for this voiceprint profile, or for all if this is null</summary>
+    public string? VoiceprintProfileId { get; set; } = null;
+
+    /// <summary>Find customizations for this recognition version, or for all if this is null</summary>
+    public SpeakerRecognizerVersion? RecognizerVersion { get; init; }
+
+    /// <summary>Find customizations for this tenant, or for all if this is null</summary>
+    public string? TenantId { get; set; }
+}
+
+public class SpeakerRecognitionCustomizationListResponse
+{
+    public required Listing<SpeakerRecognitionCustomizationDto> Customizations { get; init; }
+}
---
+++ Beey.DataExchangeModel.Common/SpeakerRecognition/v1/SpeakerRecognitionCustomizationDto.cs
@@ -0,0 +1,24 @@
+﻿using Beey.DataExchangeModel.Common.SpeakerRecognition.v1.SpeakerCatalogApi;
+using Beey.DataExchangeModel.Common.Speakers.v1;
+
+namespace Beey.DataExchangeModel.Common.SpeakerRecognition.v1;
+
+public class SpeakerRecognitionCustomizationDto
+{
+    public required int Id { get; init; }
+
+    /// <summary>Customized language (IETF tag) or null if all languages are addressed</summary>
+    public string? TargetLanguage { get; set; } = null;
+
+    /// <summary>Customized voiceprint profile or null if all profiles are addressed</summary>
+    public string? TargetVoiceprintProfileId { get; set; } = null;
+
+    /// <summary>Customized recognition version or null if all versions are addressed</summary>
+    public SpeakerRecognizerVersion? TargetRecognizerVersion { get; init; }
+
+    /// <summary>Customized scope or null if all scopes are addressed</summary>
+    public SpeakerCatalogScope? TargetScope { get; set; }
+
+    /// <summary>If model auto-generation should be disabled, or null if this customization has no effect on this setting</summary>
+    public bool? DisableModelGeneration { get; set; }
+}
