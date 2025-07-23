09b76bcdb76613317cfef746ce08b202af853ca7
Marek Boháč <bohac.marek@post.cz>
2025-07-23 11:43:08 +0200

    add CheckSanity request for Catalog

---
+++ Beey.DataExchangeModel.Common/SpeakerRecognition/v1/SpeakerCatalogApi/SpeakerRecognizeCheckSanity.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Nodes;
+using Beey.DataExchangeModel.Common.Speakers.v1;
+using Beey.DataExchangeModel.Common.SpeakerRecognition.v1.SpeakerCatalogApi;
+
+
+public class SpeakerRecognizeCheckSanityRequest
+{
+    /// <summary>Language IETF tag</summary>
+    public required string Language { get; init; }
+
+    public required string VoiceprintProfileId { get; init; }
+
+    public required SpeakerCatalogScope Scope { get; init; }
+
+    public SpeakerRecognizerVersion? RecognizerVersion { get; init; }
+}
+
+public class SpeakerRecognizeCheckSanityReply
+{
+    public required JsonObject ResponseContent { get; init; }
+}
